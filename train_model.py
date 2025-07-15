a0346a79f1cdac10f6f0340303944db6fc919254
dallast2025 <mdia140@wgu.edu>
2025-07-15 12:55:31 -0500

    completed loading and inference section

--- train_model.py
+++ train_model.py
@@ -56,7 +56,7 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = None # your code here
+model = train_model(X_train, y_train) # your code here
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
@@ -70,7 +70,7 @@ model = load_model(
 ) 
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = None # your code here
+preds = inference(model, X_test) # your code here
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
