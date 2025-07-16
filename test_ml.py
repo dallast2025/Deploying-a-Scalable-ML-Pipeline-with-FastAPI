6c69bc82b7dab00a82481f7c968057a43474b053
dallast2025 <mdia140@wgu.edu>
2025-07-16 10:42:01 -0500

    7-16-25, added to test function test one

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,19 @@
 import pytest
+from ml.model import train_model
+from sklearn.linear_model import LogisticRegression
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
     """
-    # add description for the first test
+    This test is to check if Logistic Regression model is used.
     """
     # Your code here
-    pass
+    model=train_model(X_train, y_train)
+
+    assert isinstance(model, LogisticRegression), f"{type(model)} is invalid. Please enter LogisticRegression Model."
+    
 
 
 # TODO: implement the second test. Change the function name and input as needed
